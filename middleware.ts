b5fc8a9bca916605ca143ca679eceb342eaf473d
jorbrown66 <jorben666@163.com>
2025-12-14 16:31:55 +0800

    update

--- middleware.ts
+++ middleware.ts
@@ -86,5 +86,5 @@ export const config = {
   // Match all pathnames except for
   // - … if they start with `/api`, `/_next` or `/_vercel`
   // - … the ones containing a dot (e.g. `favicon.ico`)
-  matcher: ['/', '/(zh-CN|en)/:path*', '/((?!api|_next|_vercel|.*\\..*).*)']
+  matcher: ['/((?!api|auth|_next|_vercel|.*\\..*).*)']
 };
